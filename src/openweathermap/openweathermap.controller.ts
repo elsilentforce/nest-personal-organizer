75f21df0a9b251dfa28aace8dfe7a6abd7c5314c
Patricio <native.devil@gmail.com>
2025-11-23 12:04:53 -0300

    ADD: Cache for Openweathermap API calls

--- src/openweathermap/openweathermap.controller.ts
+++ src/openweathermap/openweathermap.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { OpenweathermapService } from './openweathermap.service';
+import { env } from 'process';
 
-@Controller('openweathermap')
+@Controller('weather')
 export class OpenweathermapController {
   constructor(private readonly openweathermapService: OpenweathermapService) { }
 
   @Get()
   currentWeather() {
-    const lat: number = Number(process.env.OPENWEATHERMAP_DEFAULT_LAT);
-    const lon: number = Number(process.env.OPENWEATHERMAP_DEFAULT_LON);
+    const lat: number = Number(env.OPENWEATHERMAP_DEFAULT_LAT);
+    const lon: number = Number(env.OPENWEATHERMAP_DEFAULT_LON);
     return this.openweathermapService.getCurrentWeather(lat, lon);
   }
 }
