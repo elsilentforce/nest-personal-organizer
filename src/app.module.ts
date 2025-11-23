c96ca4d12ec1828b9227b8544c109c9ac1270b6c
Patricio <native.devil@gmail.com>
2025-11-23 16:32:45 -0300

    DELETE: Unnecesary default module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { OpenweathermapService } from './openweathermap/openweathermap.service';
 import { OpenweathermapController } from './openweathermap/openweathermap.controller';
 import { RedisService } from './redis/redis.service';
-import { RedisModule } from './redis/redis.module';
 import { OpenweathermapModule } from './openweathermap/openweathermap.module';
 
 @Module({
   imports: [OpenweathermapModule],
-  controllers: [AppController, OpenweathermapController],
-  providers: [AppService, OpenweathermapService, RedisService],
+  controllers: [OpenweathermapController],
+  providers: [OpenweathermapService, RedisService],
 })
 export class AppModule { }
