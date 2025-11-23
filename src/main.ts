d57797ea6c15b11ee27fae2ce3d2427f7273cc50
Patricio <native.devil@gmail.com>
2025-11-23 12:04:24 -0300

    ADD: Global prefix for API calls

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('/api/v1');
+  await app.listen(env.PORT ?? 3000);
 }
 bootstrap();
