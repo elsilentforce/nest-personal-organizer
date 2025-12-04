8bd005c6194675cd18e0d09938731045c312d6c0
Patricio <native.devil@gmail.com>
2025-12-04 20:48:43 -0300

    ADD: User Module

---
+++ src/user/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from 'src/generated/prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) { }
+
+  async users() {
+    return await this.prisma.user.findMany();
+  }
+
+}
